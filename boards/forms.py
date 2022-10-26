b00566f7a0e47628b873e51908870a5d1febc5d8
Mohamed-A-Badr <badr49960@gmail.com>
2022-10-26 15:45:05 +0200

    Django Forms

--- boards/forms.py
+++ boards/forms.py
@@ -1,12 +1,13 @@
-from email import message
 from django import forms
 from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea, max_length=4000)
+    message = forms.CharField(widget=forms.Textarea(
+        attrs={'rows': 5, 'placeholder': "What's in your mind?"}
+    ), max_length=4000, help_text='The max length for your message should be 4000')
     # metaData
 
-    class meta:
+    class Meta:
         model = Topic
         fields = ['subject', 'message']
--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,7 @@
+from email import message
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
+from django.urls import is_valid_path
 from .models import Board
 from django.contrib.auth.models import User
 from .models import Topic, Post
@@ -23,24 +25,42 @@ def board_topics(request, board_id):
 
 def new_topic(request, board_id):
     board = get_object_or_404(Board, pk=board_id)
+    user = User.objects.first()
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-        user = User.objects.first()
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.created_by = user
+            topic.save()
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            created_by=user
-        )
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                created_by=user,
+                topic=topic,
+            )
 
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-        return redirect('boards_topics', board_id=board.pk)
-    return render(request, 'new_topic.html', {'board': board})
+            return redirect('boards_topics', board_id=board.pk)
+    else:
+        form = NewTopicForm()
+
+        # subject = request.POST['subject']
+        # message = request.POST['message']
+        # user = User.objects.first()
+
+        # topic = Topic.objects.create(
+        #     subject=subject,
+        #     board=board,
+        #     created_by=user
+        # )
+
+        # post = Post.objects.create(
+        #     message=message,
+        #     topic=topic,
+        #     created_by=user
+        # )
+        # return redirect('boards_topics', board_id=board.pk)
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
 
 
 def about(request):
